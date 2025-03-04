1ecb976a26bba35b5ba37ffda40ff7cc1ce01ef9
pxxiionn <147414359+Pxxiionn@users.noreply.github.com>
2025-03-04 12:00:13 +0800

    upload exam

--- accessment/templatetags/custom_filters.py
+++ accessment/templatetags/custom_filters.py
@@ -10,3 +10,14 @@ def get_item(dictionary, key):
 @register.filter
 def split(value, arg):
     return value.split(arg)
+
+
+@register.filter
+def parse_correction_answer(value):
+    if not value or value == "right":
+        return {"type": "", "index": "", "text": value}
+    try:
+        parts = value.split(":")
+        return {"type": parts[0], "index": parts[1], "text": parts[2]}
+    except (ValueError, IndexError):
+        return {"type": "", "index": "", "text": ""}
