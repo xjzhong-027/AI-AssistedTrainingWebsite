71565fe380f46a8927c25147d2de5c43877ad509
pxxiionn <147414359+Pxxiionn@users.noreply.github.com>
2025-04-02 21:05:47 +0800

    题目论坛

--- accessment/models.py
+++ accessment/models.py
@@ -1,66 +1,91 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-from Account.models import Students,Class
-from ELW.models import (
-Unit,MediaMaterial,
-MainQuestion,SubQuestion,ChoiceOption,
-MatchingOption,Correction,PaperPage,TimeManagement
-)
+from django.utils import timezone
+from django import forms
+from ELW.models import MainQuestion,SubQuestion
+from Account.models import Students, Teachers
 
 
-class StudentExamRecord(models.Model):
-    user = models.ForeignKey(Students, on_delete=models.CASCADE)
-    exam = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    started_at = models.DateTimeField(auto_now_add=True)
-    ended_at = models.DateTimeField(null=True, blank=True)
-    finished_at = models.DateTimeField(null=True, blank=True)
-    submitted = models.BooleanField(default=False)
-    integrity_score = models.DecimalField(verbose_name='诚信分', max_digits=2, decimal_places=1, default=1, validators=[
-        MinValueValidator(0),  # 最小值为0
-        MaxValueValidator(1)  # 最大值为1
-    ])
-    score = models.DecimalField(verbose_name='最终总分', max_digits=5, decimal_places=2, default=0)
-
-
-class StudentPageRecord(models.Model):
-    student_exam_record = models.ForeignKey(StudentExamRecord, on_delete=models.CASCADE, related_name='page_records')
-    page = models.ForeignKey(PaperPage, on_delete=models.CASCADE, related_name='student_records')
-    submitted = models.BooleanField(default=False)
-    submitted_at = models.DateTimeField(null=True, blank=True)
-    is_expired = models.BooleanField(default=False)  # 页面是否已超时
-    remaining_time = models.FloatField(default=0,null=True, blank=True)  # 剩余时间（分钟）
-    late_score = models.DecimalField(verbose_name='逾期分', max_digits=2, decimal_places=1, default=1, validators=[
-        MinValueValidator(0),  # 最小值为0
-        MaxValueValidator(1)  # 最大值为1
-    ])
-    page_score = models.DecimalField(verbose_name='页面原始分', max_digits=5, decimal_places=2, default=0)
-    feedback = models.TextField(blank=True)
-    is_graded = models.BooleanField(default=False)
-    def __str__(self):
-        return f"Record for {self.student_exam_record.user} on Page {self.page.order}"
-
-
-class StudentAnswer(models.Model):
-    sub_question = models.ForeignKey(SubQuestion, on_delete=models.CASCADE, related_name='answers')
-    student_page_record = models.ForeignKey(StudentPageRecord, on_delete=models.CASCADE, related_name='answers')
-    text = models.TextField(verbose_name='答案文本', blank=True,null=True, default='')
-    index = models.IntegerField(verbose_name='索引', blank=True, null=True)
-    type = models.CharField(verbose_name='改错类型', max_length=50, blank=True, null=True)
-    score = models.DecimalField(verbose_name='小题得分', max_digits=5, decimal_places=2, default=0)
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    is_top = models.BooleanField(default=False)
+    top_score = models.IntegerField(default=0)#精选值
+    is_question = models.BooleanField(default=False, verbose_name="是否关于题目")
+    is_public = models.BooleanField(default=True)#是否公开，不公开则与老师私聊
+    teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
+    student = models.ForeignKey(Students, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
+    author =  models.CharField(max_length=100, null=True, blank=True)
+    is_anonymous = models.BooleanField(default=False)
+    anonymous_name = models.CharField(max_length=100, null=True, blank=True)  # 匿名名称
+    name = models.CharField(max_length=100, null=True, blank=True)  # 帖子显示的名字
+    is_announcement = models.BooleanField(default=False)  # 是否与公告
+    main_question = models.ForeignKey(MainQuestion, on_delete=models.CASCADE, null=True, blank=True,
+                                      related_name='posts')
+    sub_question = models.ForeignKey(SubQuestion, on_delete=models.CASCADE, null=True, blank=True,
+                                      related_name='posts')
 
     def __str__(self):
-        return f"Answer to {self.sub_question} by {self.student_page_record.student_exam_record.user}"
+        return self.title
+
+    def save(self, *args, **kwargs):
+        # 动态设置 name 字段
+        if self.anonymous_name:
+            self.name = self.anonymous_name
+        elif self.teacher:
+            self.name = self.teacher.name
+        elif self.student:
+            self.name = self.student.name
+        super().save(*args, **kwargs)
 
 
-class StudentMediaPlayRecord(models.Model):
-    student_exam_record = models.ForeignKey(StudentExamRecord, on_delete=models.CASCADE)
-    main_question = models.ForeignKey(MainQuestion, on_delete=models.CASCADE)
-    media_material = models.ForeignKey(MediaMaterial, on_delete=models.CASCADE,blank=True,null=True)  # 新增字段
-    play_count = models.IntegerField(default=0)
-    last_pause_time = models.FloatField(default=0)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # 关联到帖子
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
+                                       related_name='replies')  # 父评论
+    created_at = models.DateTimeField(default=timezone.now)  # 评论时间
+    content = models.TextField()  # 评论内容
+    is_top = models.BooleanField(default=False)  # 置顶
+    is_marked = models.BooleanField(default=True)  # 其他标记，默认为True
+    #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', null=True)#登陆用户名
+    teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    student = models.ForeignKey(Students, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    author = models.CharField(max_length=100, null=True, blank=True)
+    is_anonymous = models.BooleanField(default=False)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    anonymous_name = models.CharField(max_length=100, null=True, blank=True)#匿名
+
+    def save(self, *args, **kwargs):
+        # 动态设置 name 字段
+        if self.anonymous_name:
+            self.name = self.anonymous_name
+        elif self.student:
+            self.name = self.student.name
+        elif self.teacher:
+            self.name = self.teacher.name
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Play record for {self.student_exam_record.user} on {self.main_question}"
+        return f"Comment on {self.post.title} by {self.name}"
+
+
+class Anonymous(models.Model):
+    user = models.ForeignKey(Students, on_delete=models.CASCADE, related_name='anonymous_names')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='anonymous_posts')
+    anonymous_name = models.CharField(max_length=50)
 
     class Meta:
-        unique_together = ('student_exam_record', 'main_question', 'media_material')
+        unique_together = (('user', 'post'),)
+
+    def __str__(self):
+        return self.anonymous_name
+
+
+
+
+
+
+
+
